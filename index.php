aafeda21204d9bf4d7c739a4ec6853f52c803b3d
10375587 <39301239+10375587@users.noreply.github.com>
2018-05-15 13:07:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
 
 <?php
 echo "Hello World!";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
 ?>
+  
 
 </body>
 </html>
